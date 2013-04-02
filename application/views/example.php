2e59fe7b49d28b773906b88f74e886a417a9707f
vincent728 <daudivincent@yahoo.com>
2013-04-02 17:02:49 +0300

    a tab for calendar added on logged in panel

--- application/views/example.php
+++ application/views/example.php
@@ -37,6 +37,7 @@ a:hover
                 ?>
                  <a href='<?php echo site_url('examples/clients_management')?>'>Clients</a> |
                 <a href='<?php echo site_url('examples/events_management')?>'>Event(s)</a> |   
+                <a href='<?php echo site_url('calendar/index')?>'>Calendar</a> |   
                 <a href='<?php echo site_url('auth/logout')?>'>logout</a>  |
               <?php    
                 }if($this->ion_auth->logged_in()==TRUE && $this->ion_auth->in_group($group=1)){
